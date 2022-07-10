1992b5dfc5eb31845756beca564a7bc7689ef67d
Prathammehra7 <manshumehra7@gmail.com>
2022-07-10 13:37:14 +0530

    first

---
+++ src/Card3.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import "./Card.css"
+// import luxury from "./luxury.png";
+export default function Card3()
+{   
+    return(
+        <>
+          <div className=" w-64 h-96 p-7 pr-12 bg-[#00403F] rounded-r-xl Mbrb "  >
+        <div className=" ">
+            <h3 className="pt-3.5 pb-3.5 text-2xl uppercase  font-Dosia font-semibold	">third</h3>
+            <p className="pt-3.5 pb-3.5 text-sm text-white font-semibold">Wikipedia is hosted by the Wikimedia Foundation, a non-profit organization that also hosts a range of other projects. </p>
+            <button className="text-[#023230] bg-white p-3 w-32 rounded-full font-semibold mt-5 ">Click</button>
+        </div>
+    </div>
+        </>
+    ); 
+}
